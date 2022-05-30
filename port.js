9c01883c979c1ce9b161803ffff5eefbabdb0266
Kaviyarasu <100459234+S-kaviyarasu@users.noreply.github.com>
2022-05-30 21:54:10 +0530

    personal portfolio

---
+++ port.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if(this.scrollY>20){
+                $(".navbar").addClass("sticky");
+        }
+        else{
+            $(".navbar").removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+        
+    });
+    //slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+
+    // toggle menu/navbar script
+    $('.menu-button').click(function(){
+        $('.navbar .menu').toggleClass('active');
+        $('.menu-button i').toggleClass('active');
+
+    })
+   
+});
+//typing animation script
+// var typed = new Typed(".typing",{
+//     strings: ["Youtuber", "Developer", "Blogger", "Designer"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true
+// });
